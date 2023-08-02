7a31afe781f4aeb9f64224a32050c02ec7d090d8
sravan k <sravankakireddy@gmail.com>
2023-08-02 15:59:27 +0530

    roboshop.tf

---
+++ roboshop-v1/roboshop.tf
@@ -0,0 +1,100 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0ea14648eaf6add85"]
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "mongodb" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0ea14648eaf6add85"]
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+
+resource "aws_instance" "catalogue" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0ea14648eaf6add85"]
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+resource "aws_instance" "redis" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0ea14648eaf6add85"]
+
+  tags = {
+    Name = "redis"
+  }
+}
+
+resource "aws_instance" "user" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0ea14648eaf6add85"]
+
+  tags = {
+    Name = "user"
+  }
+}
+
+resource "aws_instance" "cart" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0ea14648eaf6add85"]
+
+  tags = {
+    Name = "cart"
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0ea14648eaf6add85"]
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_instance" "shipping" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0ea14648eaf6add85"]
+
+  tags = {
+    Name = "shipping"
+  }
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0ea14648eaf6add85"]
+
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+resource "aws_instance" "payment" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0ea14648eaf6add85"]
+
+  tags = {
+    Name = "payment"
+  }
+}
+
