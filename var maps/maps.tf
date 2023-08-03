fd41c5d8045b62ae498d12fac0eb31731ac8903a
sravan k <sravankakireddy@gmail.com>
2023-08-03 17:27:23 +0530

    maps

---
+++ var maps/maps.tf
@@ -0,0 +1,15 @@
+variable "fruit_stock_with_price" {
+  default = {
+    apple={
+      stock =200
+      price=2
+    }
+    bananna={
+      stock =400
+      price=5
+    }
+}
+}
+output "fruit_stock_apple" {
+  value = var.fruit_stock_with_price[apple]
+}
