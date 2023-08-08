53b65e7b0df2b25e0c01e472bbc8cbfe081507dd
sravan k <sravankakireddy@gmail.com>
2023-08-08 18:46:27 +0530

    terrform dev-prod

---
+++ state/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket = "sdevops-28"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+variable "test" {
+  default = sravan
+}
+output "hello" {
+  value = "var.test"
+}
