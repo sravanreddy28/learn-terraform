9bbc3cb9fc33e6b037bcc59d14ebc5e66b31f247
sravan k <sravankakireddy@gmail.com>
2023-09-07 16:46:23 +0530

    ansible

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -29,7 +29,8 @@ resource "null_resource" "ansible" {
     command = <<EOF
 cd/home/centos/roboshop-ansible
 git pull
-ansible-playbook -i ${var.name}-dev.sdevops28.online, -e ansible_user=centos -e ansible_password=DevOps321 main.yml
+sleep 30
+ansible-playbook -i ${var.name}-dev.sdevops28.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
 EOF
   }
 }
