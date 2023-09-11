e41264e501f60e832fdab940d73c078a559ae8f1
sravan k <sravankakireddy@gmail.com>
2023-09-11 12:18:50 +0530

    ansible roles

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -27,10 +27,10 @@ resource "null_resource" "ansible" {
 
   provisioner "local-exec" {
     command = <<EOF
-cd /roboshop-ansible-roles/
+cd /home/centos/roboshop-ansible-roles
 git pull
 sleep 30
-ansible-playbook -i ${var.name}-dev.sdevops28.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+ansible-playbook -i ${var.name}-dev.sdevops28.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component= ${var.name}
 EOF
   }
 }
